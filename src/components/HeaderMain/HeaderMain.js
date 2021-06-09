5afa36f2071bf05ada39959181ac7a6797f9ef5f
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-09 20:15:33 +0600

    update house image card

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -7,10 +7,10 @@ const HeaderMain = () => {
     }
     return (
         <div className="bg-white h-screen flex flex-col justify-center items-center headerImage">
-            <div class="bg-auto bg-no-repeat bg-center ..." >
+            <div className="bg-auto bg-no-repeat bg-center ..." >
                 <h3 className="lg:text:7xl md:text-5xl sm:text-3xl text-3xl font-bold mb-14 uppercase text-white">Find your house rent</h3>
                 <form onSubmit={handleSubmit} className="flex justify-center">
-                    <input class="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300 py-2 px-4 rounded w-3/5" placeholder="Search..." required />
+                    <input className="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300 py-2 px-4 rounded w-3/5" placeholder="Search..." required />
                     <button type="submit" className="text-white ml-4 bg-green-800  py-2 px-8 rounded">
                         Find Now
                     </button>
--- src/components/HouseRent/HouseRent.js
+++ src/components/HouseRent/HouseRent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import house1 from '../../images/house-1.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faBed, faToiletPaper } from '@fortawesome/free-solid-svg-icons';
 import './HouseRent.css';
 
 const HouseRent = () => {
@@ -13,60 +15,87 @@ const HouseRent = () => {
                     <h2>available today</h2> 
                 </div>
             </div>
-            <div className="flex flex-wrap -m-4">
-                <div class="p-4 sm:w-1/2 lg:w-1/3">
-                    <div class="md:flex md:flex-col house-card">
-                        <div class="">
-                            <img class="rounded-lg max-w-full h-auto" src={house1} alt="Woman paying for a purchase" />
+            <div className="flex flex-wrap -m-4 mt-5 pt-5">
+                <div className="p-4 sm:w-1/2 lg:w-1/3">
+                    <div className="md:flex md:flex-col house-card">
+                        <div className="">
+                            <img className="rounded-lg max-w-full h-auto" src={house1} alt="Woman paying for a purchase" />
                         </div>
-                        <div class="mt-4 md:mt-0 bg-white px-6">
-                            <div class="uppercase tracking-wide text-sm house-title font-extrabold mt-3 text-lg">Washington Avenue</div>
-                            <p class="mt-2 text-gray-600">Nasirabad H/S, Chattagram</p>
-                            <div className="flex text-gray-600 mt-2 mb-4 pb-2 justify-between pr-10">
-                                <span>3 Bedrooms</span>
-                                <span className="ml-2">2 Bathroom</span>
+                        <div className="mt-4 md:mt-0 bg-white px-6">
+                            <div className="uppercase tracking-wide text-sm house-title font-extrabold mt-3 text-lg">Washington Avenue</div>
+                            <p className="mt-2 text-gray-600">
+                                <FontAwesomeIcon icon={faMapMarkerAlt} />
+                                <span className="ml-2">Nasirabad H/S, Chattagram</span>
+                            </p>
+                            <div className="flex text-gray-600 mt-2 mb-5 pb-4 justify-between pr-10">
+                                <p>
+                                    <FontAwesomeIcon icon={faBed} />
+                                    <span className="ml-2">3 Bedrooms</span>
+                                </p>
+                                <p className="ml-2">
+                                    <FontAwesomeIcon icon={faToiletPaper} />
+                                    <span className="ml-2">2 Bathroom</span>
+                                </p>
                             </div>
                             <div className="flex text-gray-600 mt-2 mb-4 justify-between">
                                 <span className="font-bold text-green-800 text-3xl">$194</span>
-                                <button className="text-white ml-4 bg-green-800  py-2 px-8 rounded">View Details</button>
+                                <button className="text-white ml-4 bg-green-800 hover:bg-green-700 py-2 px-8 rounded">View Details</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="p-4 sm:w-1/2 lg:w-1/3">
-                    <div class="md:flex md:flex-col house-card">
-                        <div class="">
-                            <img class="rounded-lg max-w-full h-auto" src={house1} alt="Woman paying for a purchase" />
+                <div className="p-4 sm:w-1/2 lg:w-1/3">
+                    <div className="md:flex md:flex-col house-card">
+                        <div className="">
+                            <img className="rounded-lg max-w-full h-auto" src={house1} alt="Woman paying for a purchase" />
                         </div>
-                        <div class="mt-4 md:mt-0">
-                            <div class="uppercase tracking-wide text-sm text-black font-bold mt-2 text-base">Washington Avenue</div>
-                            <p class="mt-2 text-gray-600">Nasirabad H/S, Chattagram</p>
-                            <div className="flex text-gray-600 mt-2 mb-4 pb-2">
-                                <span>3 Bedrooms</span>
-                                <span className="ml-2">2 Bathroom</span>
+                        <div className="mt-4 md:mt-0 bg-white px-6">
+                            <div className="uppercase tracking-wide text-sm house-title font-extrabold mt-3 text-lg">Washington Avenue</div>
+                            <p className="mt-2 text-gray-600">
+                                <FontAwesomeIcon icon={faMapMarkerAlt} />
+                                <span className="ml-2">Nasirabad H/S, Chattagram</span>
+                            </p>
+                            <div className="flex text-gray-600 mt-2 mb-5 pb-4 justify-between pr-10">
+                                <p>
+                                    <FontAwesomeIcon icon={faBed} />
+                                    <span className="ml-2">3 Bedrooms</span>
+                                </p>
+                                <p className="ml-2">
+                                    <FontAwesomeIcon icon={faToiletPaper} />
+                                    <span className="ml-2">2 Bathroom</span>
+                                </p>
                             </div>
-                            <div className="flex text-gray-600 mt-2 mb-4">
-                                <span>$194</span>
-                                <button className="text-white ml-4 bg-green-800  py-2 px-8 rounded">View Details</button>
+                            <div className="flex text-gray-600 mt-2 mb-4 justify-between">
+                                <span className="font-bold text-green-800 text-3xl">$194</span>
+                                <button className="text-white ml-4 bg-green-800 hover:bg-green-700 py-2 px-8 rounded">View Details</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="p-4 sm:w-1/2 lg:w-1/3">
-                    <div class="md:flex md:flex-col house-card">
-                        <div class="">
-                            <img class="rounded-lg max-w-full h-auto" src={house1} alt="Woman paying for a purchase" />
+                <div className="p-4 sm:w-1/2 lg:w-1/3">
+                    <div className="md:flex md:flex-col house-card">
+                        <div className="">
+                            <img className="rounded-lg max-w-full h-auto" src={house1} alt="Woman paying for a purchase" />
                         </div>
-                        <div class="mt-4 md:mt-0">
-                            <div class="uppercase tracking-wide text-sm text-black font-bold mt-2 text-base">Washington Avenue</div>
-                            <p class="mt-2 text-gray-600">Nasirabad H/S, Chattagram</p>
-                            <div className="flex text-gray-600 mt-2 mb-4 pb-2">
-                                <span>3 Bedrooms</span>
-                                <span className="ml-2">2 Bathroom</span>
+                        <div className="mt-4 md:mt-0 bg-white px-6">
+                            <div className="uppercase tracking-wide text-sm house-title font-extrabold mt-3 text-lg">Washington Avenue</div>
+                            <p className="mt-2 text-gray-600">
+                                <FontAwesomeIcon icon={faMapMarkerAlt} />
+                                <span className="ml-2">Nasirabad H/S, Chattagram</span>
+                            </p>
+                            <div className="flex text-gray-600 mt-2 mb-5 pb-4 justify-between pr-10">
+                                <p>
+                                    <FontAwesomeIcon icon={faBed} />
+                                    <span className="ml-2">3 Bedrooms</span>
+                                </p>
+                                <p className="ml-2">
+                                    <FontAwesomeIcon icon={faToiletPaper} />
+                                    <span className="ml-2">2 Bathroom</span>
+                                </p>
                             </div>
-                            <div className="flex text-gray-600 mt-2 mb-4">
-                                <span>$194</span>
-                                <button className="text-white ml-4 bg-green-800  py-2 px-8 rounded">View Details</button>
+                            <div className="flex text-gray-600 mt-2 mb-4 justify-between">
+                                <span className="font-bold text-green-800 text-3xl">$194</span>
+                                <button className="text-white ml-4 bg-green-800 hover:bg-green-700 py-2 px-8 rounded">View Details</button>
                             </div>
                         </div>
                     </div>
