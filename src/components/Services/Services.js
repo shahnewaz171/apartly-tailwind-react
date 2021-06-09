17978564043ea98b1bec9b9b3fea689d48da8b87
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-09 23:21:55 +0600

    update services section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import house1 from '../../images/house-1.png';
 import apartment from '../../images/logos/apartment.png';
 import affordable from '../../images/logos/affordable.png';
 import lessee from '../../images/logos/lessee.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt, faBed, faToiletPaper } from '@fortawesome/free-solid-svg-icons';
 import './Services.css';
 
 const Services = () => {
     return (
         <div className="md:px-20">
-            <div className="mt-5 flex flex-col justify-center items-center mb-12 mt-20">
-                <h6 className="font-semibold text-green-800">Service</h6>
-                <div className="text-3xl font-extrabold text-green-900 flex flex-col items-center">
-                    <h2 className="mb-1">We're an agency tailored to all</h2>
-                    <h2>clients' needs that always delivers</h2> 
+            <div className="mt-5 md:flex md:flex-col justify-center items-center mb-12 mt-20">
+                <h6 className="font-semibold text-green-800 text-center">Service</h6>
+                <div className="text-3xl font-extrabold text-green-900 md:flex md:flex-col items-center px-4">
+                    <h2 className="mb-1 text-center">We're an agency tailored to all</h2>
+                    <h2 className="text-center">clients' needs that always delivers</h2> 
                 </div>
             </div>
             <div className="flex flex-wrap -m-4 mt-5 pt-5">
                 <div className="p-4 sm:w-1/2 lg:w-1/3">
                     <div className="md:flex md:flex-col house-card justify-center items-center">
                         <div className="">
-                            <img className="rounded-lg w-20 h-auto" src={apartment} alt="Woman paying for a purchase" />
+                            <img className="rounded-lg w-20 h-auto service-img" src={apartment} alt="Woman paying for a purchase" />
                         </div>
-                        <div className="mt-4 md:mt-0 px-6 flex-col flex justify-center items-center">
+                        <div className="mt-4 md:mt-0 md:px-6 flex-col flex justify-center items-center">
                             <div className="tracking-wide text-sm house-title font-bold mt-3 text-lg">Wide Range of Properties</div>
-                            <p className="mt-2 text-gray-600 px-8 text-center">
+                            <p className="mt-2 text-gray-600 md:px-8 text-center">
                                 With a robust selection of popular properties on hand , as well as leading properties from experts.
                             </p>
                         </div>
@@ -34,11 +31,11 @@ const Services = () => {
                 <div className="p-4 sm:w-1/2 lg:w-1/3">
                     <div className="md:flex md:flex-col house-card justify-center items-center">
                         <div className="">
-                            <img className="rounded-lg w-20 h-auto" src={affordable} alt="Woman paying for a purchase" />
+                            <img className="rounded-lg w-20 h-auto service-img" src={affordable} alt="Woman paying for a purchase" />
                         </div>
-                        <div className="mt-4 md:mt-0 px-6 flex-col flex justify-center items-center">
+                        <div className="mt-4 md:mt-0 md:px-6 flex-col flex justify-center items-center">
                             <div className="tracking-wide text-sm house-title font-bold mt-3 text-lg">Financing Made Easy</div>
-                            <p className="mt-2 text-gray-600 px-8 text-center">
+                            <p className="mt-2 text-gray-600 md:px-8 text-center">
                                 Our stress-free finance department that can find financial solutions to save you money.
                             </p>
                         </div>
@@ -47,11 +44,11 @@ const Services = () => {
                 <div className="p-4 sm:w-1/2 lg:w-1/3">
                     <div className="md:flex md:flex-col house-card justify-center items-center">
                         <div className="">
-                            <img className="rounded-lg w-20 h-auto" src={lessee} alt="Woman paying for a purchase" />
+                            <img className="rounded-lg w-20 h-auto service-img" src={lessee} alt="Woman paying for a purchase" />
                         </div>
-                        <div className="mt-4 md:mt-0 px-6 flex-col flex justify-center items-center">
+                        <div className="mt-4 md:mt-0 md:px-6 flex-col flex justify-center items-center">
                             <div className="tracking-wide text-sm house-title font-bold mt-3 text-lg">Trusted by Thousands</div>
-                            <p className="mt-2 text-gray-600 px-8 text-center">
+                            <p className="mt-2 text-gray-600 md:px-8 text-center">
                                 10 new offers every day. 350 offers on site, trusted by a community of thousands of users.
                             </p>
                         </div>
