4b389ea95da3c843e5dddca4c1822e81810e1a13
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-09 15:09:51 +0600

    install react router dom and tailwind css

---
+++ src/components/HeaderMain/HeaderMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HeaderMain.css';
+
+const HeaderMain = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeaderMain;
